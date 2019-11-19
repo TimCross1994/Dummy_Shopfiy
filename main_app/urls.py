962f5cac838a6506f38b946f7204a3486949aafb
Timothy Cross <timothycross1994@gmail.com>
2019-11-19 14:32:14 -0800

    create functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('product_form/', views.product_form, name='product'),
+    path('add/', views.CreateProduct.as_view(), name='add_product'),
 ]
