db527e55693e4fdba234f62f37669045f6012c46
Timothy Cross <timothycross1994@gmail.com>
2019-11-19 17:35:29 -0800

    adding a navbar

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('product_form/', views.product_form, name='product'),
-    path('add/', views.CreateProduct.as_view(), name='add_product'),
+    path('', views.product_form, name='products'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,15 +4,7 @@ from .models import Search
 from django.http import HttpResponse
 
 
-def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
-
 def product_form(request):
     search = Search.objects.all()
     return render(request, 'product_form.html')
 
-class CreateProduct(CreateView):
-    model= Search
-    fields = '__all__'
-    template_name = 'add.html'
-    success_url = '/product_form'
